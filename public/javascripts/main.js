136cbe1c0e6631963dfc2746a067e533e01f7611
Rich Middleditch <richmiddleditch@gmail.com>
2013-05-30 10:44:06 +0100

    adding uniques

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,11 +10,17 @@ socket.on('all', function (data) {
     console.log(data);
     var sum = 0, i = 0;
 
-    for(i; i < data.timers.revue.times.parsing.length; i++){
-        sum += data.timers.revue.times.parsing[i];
+    for(i; i < data.timers.bbc.tviplayer.iplayer.index.render_time.length; i++){
+        sum += data.timers.bbc.tviplayer.iplayer.index.render_time[i];
     }
 
     if (sum) {
-        $('.parsing h1').text(Math.round(sum / data.timers.revue.times.parsing.length) + 'ms').stop().fadeIn(0).fadeOut(10000);
+        $('.parsing h1').text(Math.round(sum / data.timers.bbc.tviplayer.iplayer.index.render_time.length) + 'ms').stop().fadeIn(0).fadeOut(10000);
+    }
+
+    var uniques = data.sets.bbc.tviplayer.iplayer.index.unique_browsers.store.length;
+
+    if (uniques) {
+        $('.uniques h1').text(uniques).stop().fadeIn(0).fadeOut(10000);
     }
 });
